884d83fd04cc2139ceabbdbd9af5951d2bd32b02
Francis <francisventura1@hotmail.com>
2021-02-01 14:48:28 -0500

    initial (not complete)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    render(){
+        return (<div id="app">
+            <h1>Not PONG</h1>
+            <Board />
+        </div>);
+    }
+}
+
+class Board extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.leftPaddleTop = 0;
+        this.rightPaddleTop = 0;
+
+        this.movePaddle = this.movePaddle.bind(this);
+    }
+
+    movePaddle(event){
+        var leftPaddle = document.getElementById('left-paddle');
+        var rightPaddle = document.getElementById('right-paddle');
+
+        console.log(event.keyCode);
+        if(event.keyCode === 83){
+            this.leftPaddleTop -= 20;
+            leftPaddle.style.top = this.leftPaddleTop + 'px';
+        }
+        if(event.keyCode === 88){
+            this.leftPaddleTop += 20;
+            leftPaddle.style.top = this.leftPaddleTop + 'px';
+        }
+        if(event.keyCode === 38){
+            this.rightPaddleTop -= 20;
+            rightPaddle.style.top = this.rightPaddleTop + 'px';
+        }
+        if(event.keyCode === 40){
+            this.rightPaddleTop += 20;
+            rightPaddle.style.top = this.rightPaddleTop + 'px';
+        }
+
+
+
+    }
+
+    
+    render(){
+        return (<div id="board" onKeyDown={this.movePaddle} tabIndex='0'>
+           <div id="left-paddle"></div>
+            <div id="right-paddle"></div>
+        </div>);
+    }
+}
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
