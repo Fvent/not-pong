e9d77a0dbdeea44b93aa8878bd783962d1f6e906
Francis <francisventura1@hotmail.com>
2021-02-12 11:53:04 -0500

    added a button that starts movement

--- src/index.js
+++ src/index.js
@@ -18,16 +18,26 @@ class Board extends React.Component {
     constructor(props){
         super(props);
 
+        this.state = {
+            gameStarted : false,
+            direction: "left"
+        }
+
         this.leftPaddleTop = 0;
         this.rightPaddleTop = 0;
 
+        this.puckLeft = 38;
+        this.puckTop = 33;
+
         this.movePaddle = this.movePaddle.bind(this);
+        this.handleStartButton = this.handleStartButton.bind(this);
+        this.movingPuckFunction = this.movingPuckFunction.bind(this);
     }
 
     movePaddle(event){
         var leftPaddle = document.getElementById('left-paddle');
         var rightPaddle = document.getElementById('right-paddle');
-
+        
         console.log(event);
         if(event.keyCode === 83){
             if(this.leftPaddleTop > 0){
@@ -54,17 +64,39 @@ class Board extends React.Component {
             }
            
         }
-
-
-
     }
 
-    
+    handleStartButton(){
+        this.setState({
+            gameStarted: true,
+            direction: "left"
+        });
+        setInterval(this.movingPuckFunction, 5);
+    }
+
+   movingPuckFunction(){
+
+        var puck = document.getElementById('puck');
+
+        var puckMovement = 0.1;
+
+        if(this.state.direction === "left"){
+                this.puckLeft -= puckMovement;
+                puck.style.left = this.puckLeft + 'vw';
+        }
+        if(this.state.direction === "right"){
+                this.puckLeft += puckMovement;
+                puck.style.left = this.puckLeft + 'vw';
+        }
+   }
+
+
     render(){
         return (<div id="board" onKeyDown={this.movePaddle} tabIndex='0'>
            <div id="left-paddle"></div>
            <div id="puck"></div>
             <div id="right-paddle"></div>
+            <button id="start-btn" onClick={this.handleStartButton}>Start</button>
         </div>);
     }
 }
